5663e36da590f57fdf4ba43cff555c30ad7e286a
Shery <sherynayak@gmail.com>
2024-06-21 11:28:19 +0530

    first commit

---
+++ test_login.py
@@ -0,0 +1,14 @@
+import pytest
+
+
+def testLogin():
+    print("Login Sucessfully")
+
+def testCalc():
+    assert 2+2 == 4
+
+# @pytest.mark.sanity
+def testLogoff():
+    print("Logoff Sucessfully")
+
+    
